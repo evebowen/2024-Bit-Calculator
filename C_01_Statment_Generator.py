e6e4c018bc2590b93c7f3c9ba45903d769145c17
evebowen <161265942+evebowen@users.noreply.github.com>
2024-03-18 14:42:32 +1300

    supposed to be a decoration test
    but it doesn't work and I don't know why

---
+++ C_01_Statment_Generator.py
@@ -0,0 +1,7 @@
+# functions go here
+
+def statment_genorator(statment, decoration):
+    print(f"\n{decoration * 5} {statment} {decoration * 5}")
+
+# Main Routine goes here
+statment_genorator (statment: "insructions", decoration "-")
